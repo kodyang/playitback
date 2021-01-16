20749696a067cbc7d4e57f1b6d5fdf71125edec0
torja99.github.io <istiaqum@mcmaster.ca>
2021-01-16 15:38:49 -0500

    download part complete

--- test_listen_api/listen_api_url.js
+++ test_listen_api/listen_api_url.js
@@ -1,35 +1,61 @@
-var fetch = require('node-fetch'); const { get } = require('request');
+const path = require("path");
+const fs = require('fs');
+const { https } = require('follow-redirects');
 
-var url = 'https://listen-api.listennotes.com/api/v2/search?q=star%20wars&sort_by_date=0&type=episode&offset=0&len_min=10&len_max=30&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0';
+var fetch = require('node-fetch');
+
+var eps_title = 'ball'
 
 var key = '0ac87b1a52154a49ab07451d34224f2b';
 
-//change using MDN documentation
-async function getData(url) {
+//function that can make request
+async function getData(eps_title) {
+  let url = 'https://listen-api.listennotes.com/api/v2/search?q=' + eps_title + '&sort_by_date=0&type=episode&offset=0&len_min=0&len_max=10&genre_ids=68%2C82&published_before=1580172454000&published_after=0&only_in=title%2Cdescription&language=English&safe_mode=0'
+
   const response = await fetch(url, {
     method: 'GET',
     headers: {
       'X-ListenAPI-Key': key
     }
-  });
+  }).catch(error => console.log(error));
   return response.json();
 
 }
-getData(url).then(data => {
 
-  console.log(data);
-  console.log(data.results[0].audio)
 
+// function that returns audio file 
+function urlToMp3(url, title) {
+  let file = fs.createWriteStream(path.join(__dirname, './downloads', title + '.flac'));
+  https.get(url, function (response) {
+    response.pipe(file);
+  });
+}
+
+//call listen notes API using getData function
+getData(eps_title).then(data => {
+
+  //  console.log('count: ' + data.count);
 
   //2 loops through results and gives back all of the audio urls
   data.results.forEach(function (result) {
-    // find img src
-    var audioUrl = result.audio;
-    console.log(audioUrl);
+    let audioUrl = result.audio;
+
+    //title is id of podcast also extension of mp3 file
+    let title = result.id;
+    //let title = result.title_original;
+
+    // console.log(audioUrl);
+    console.log(title);
+    urlToMp3(audioUrl, title);
 
   });
 
 });
 
+//handles errors
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
 
-//3 function that returns audio file 
+
+
